0940b38ba9ad6c25b5359d77be9186a5572e71de
Paul Grayson <paul@pololu.com>
2024-03-14 09:18:54 -0700

    remove comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,3 @@
-# home-manager build
-# home-manager switch
-
 { config, pkgs, ... }:
 
 with {
@@ -33,9 +30,6 @@ with {
     ]))
   ];
 
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".nethackrc".source = dotfiles/nethackrc;
     ".bashrc".source = dotfiles/bashrc.sh;
@@ -45,33 +39,7 @@ with {
     ".emacs.d/init.el".source = link dotfiles/init.el;
     ".emacs.d/custom_file.el".source = link dotfiles/custom_file.el;
     ".config/htop/htoprc".source = dotfiles/htoprc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/paul/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
