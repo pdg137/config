393a09eef8b4264a75fdfcb72560119a4a5fadd3
Paul Grayson <pdg@alum.mit.edu>
2024-03-16 00:18:15 -0700

    also load nix.sh

--- home-manager/dotfiles/bashrc.sh
+++ home-manager/dotfiles/bashrc.sh
@@ -9,7 +9,9 @@ shopt -s histappend
 
 if [ -e /home/paul/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
     source /home/paul/.nix-profile/etc/profile.d/hm-session-vars.sh
-elif [ -e /home/paul/.nix-profile/etc/profile.d/nix.sh ]; then
+fi
+
+if [ -e /home/paul/.nix-profile/etc/profile.d/nix.sh ]; then
     source /home/paul/.nix-profile/etc/profile.d/nix.sh
 fi
 
