9813689b7e8d86431497709ec7a111092e68b821
Paul Grayson <pdg@alum.mit.edu>
2024-03-22 08:54:16 -0700

    simplify profile, fix nethackrc

--- home-manager/dotfiles/profile.sh
+++ home-manager/dotfiles/profile.sh
@@ -1,11 +1,8 @@
 # if running bash
 if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
     if [ -f "$HOME/.bashrc" ]; then
         . "$HOME/.bashrc"
     fi
 fi
 
-export PATH=$PATH:"/opt/microchip/xc8/v1.33/bin"
-
-if [ -e /home/paul/.nix-profile/etc/profile.d/nix.sh ]; then . /home/paul/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+echo "hi"
