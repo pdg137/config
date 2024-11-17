ab8710515d2009eafeb168118d813bf2a0c3c228
Paul Grayson <pdg@alum.mit.edu>
2024-11-17 14:13:21 -0800

    set NIX_PATH in switch

--- home-manager/switch.nix
+++ home-manager/switch.nix
@@ -4,6 +4,7 @@ let
 
   # Otherwise, use nixos-24.05 from 2024-09-29:
   nixpkgs = fetchTarball {
+    name = "nixpkgs";
     url = "https://github.com/NixOS/nixpkgs/archive/fbca5e7.tar.gz";
     sha256 = "07wa6y7q4ql0x1jj08dignak2lra003inf2cxl4xxvyqdsspshp3";
   };
@@ -36,6 +37,7 @@ let
         echo "Config version: $CONFIG_VERSION"
         read -p "Press enter to switch configuration to ${file}..."
         export HOME_MANAGER_CONFIG="${file}"
+        export NIX_PATH=nixpkgs="${nixpkgs}"
         ${home-manager}/bin/home-manager switch'';
     in
       pkgs.stdenv.mkDerivation {
